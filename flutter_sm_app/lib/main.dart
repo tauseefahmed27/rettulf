570cc9e2e8cd99590bedce862ea47d29e52f604d
tauseefahmed27 <stauseef.09@gmail.com>
2021-04-18 16:29:52 +0530

    first commit

---
+++ flutter_sm_app/lib/main.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(LmsApp());
+}
+
+class LmsApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var materialApp = MaterialApp(
+      title: "Leave Managment System",
+      home: HomePage(),
+    );
+    return materialApp;
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedItemIndex = 0; //changeable only for functionality purpose
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: Row(
+        children: <Widget>[
+          buildButtonBar(
+            Icons.home,
+            0,
+          ),
+          buildButtonBar(
+            Icons.add,
+            1,
+          ),
+          buildButtonBar(
+            Icons.person,
+            2,
+          ),
+        ],
+      ),
+      appBar: AppBar(
+        title: Text("Navigation Page"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text(
+          "Funcionality Check: $_selectedItemIndex",
+          style: TextStyle(fontSize: 26),
+        ),
+      ),
+    );
+  }
+
+  Widget buildButtonBar(
+    IconData icon,
+    int index,
+  ) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          _selectedItemIndex = index;
+        });
+      },
+      child: Container(
+        height: 60,
+        width: MediaQuery.of(context).size.width / 3,
+        decoration: BoxDecoration(
+          color: index == _selectedItemIndex
+              ? Colors.lightBlue.shade300
+              : Colors.white,
+        ),
+        child: Icon(
+          icon,
+          color: index == _selectedItemIndex ? Colors.black : Colors.grey,
+        ),
+      ),
+    );
+  }
+}
